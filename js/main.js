6560668393d35d11de273972926b69ffc62331dc
sameh <samehmeligy3629@gmail.com>
2024-06-03 10:20:36 +0300

    merge

--- js/main.js
+++ js/main.js
@@ -1,125 +1,104 @@
-let menu = document.querySelector(".menu")
-menu.addEventListener("click", function (e) {
-    document.querySelector(".links").classList.toggle("active")
-})
-// 
-let color = localStorage.getItem("color")
-let linksG = document.querySelectorAll(".LinksFoof ul li")
-let linksGsa = document.querySelectorAll(".LinksFoof ul li a")
-// 
-if (color !== null) {
-    document.documentElement.style.setProperty("--dark-orange", color)
-} else if (color == "undefined") {
-}
-let nightlocal = localStorage.getItem("nightMode")
-if (nightlocal === "darkMode") {
-    let body = document.querySelector("body")
-    body.classList.add("darkMode")
-} else {
-    let body = document.querySelector("body")
-    body.classList.remove("darkMode")
-}
-menu.addEventListener("click", function (e) {
-    menu.classList.toggle("active")
-})
+// Menu toggle for mobile view
+let menu = document.querySelector(".menu");
+menu.addEventListener("click", function () {
+    document.querySelector(".links").classList.toggle("active");
+    menu.classList.toggle("active");
+});
 
-let header = document.querySelector("header")
-let btnTop = document.querySelector(".topBtn")
+// Theme color and night mode settings
+let color = localStorage.getItem("color");
+let nightMode = localStorage.getItem("nightMode");
+const body = document.querySelector("body");
+
+if (color) {
+    document.documentElement.style.setProperty("--dark-orange", color);
+}
+
+if (nightMode === "darkMode") {
+    body.classList.add("darkMode");
+} else {
+    body.classList.remove("darkMode");
+}
+
+// Scroll effects for header and back-to-top button
+let header = document.querySelector("header");
+let btnTop = document.querySelector(".topBtn");
 window.addEventListener("scroll", function () {
     if (window.scrollY > 100) {
-        header.classList.add("active")
-        btnTop.classList.add("active")
+        header.classList.add("active");
+        btnTop.classList.add("active");
     } else {
-        btnTop.classList.remove("active")
-        header.classList.remove("active")
+        header.classList.remove("active");
+        btnTop.classList.remove("active");
     }
-})
-// 
-let searchBtn = document.querySelector(".search i")
-let search = document.querySelector(".searchPage")
+});
+
+// Search functionality
+let searchBtn = document.querySelector(".search i");
+let search = document.querySelector(".searchPage");
 searchBtn.addEventListener("click", function () {
-    search.classList.toggle("active")
-})
+    search.classList.toggle("active");
+});
 search.addEventListener("click", function (e) {
-    e.target.classList.remove("active")
-})
+    e.target.classList.remove("active");
+});
 
+// Navigation color change and night mode toggle
+let navBar = document.querySelector(".navColor .afterNa");
+let navOpen = document.querySelector(".navColor");
+let span = document.querySelectorAll(".navColor .span span");
+let night = document.querySelector(".navNight");
 
-linksG.forEach(function (e) {
-    e.addEventListener("click", function () {
-        linksG.forEach(function (s) {
-            s.classList.remove("active");
-        })
-        this.classList.add("active")
-
-    })
-})
-
-let navBar = document.querySelector(".navColor .afterNa")
-let navOpen = document.querySelector(".navColor")
-let span = document.querySelectorAll(".navColor .span span")
-let night = document.querySelector(".navNight")
-let body = document.querySelector("body")
 navBar.addEventListener("click", function () {
-    navOpen.classList.toggle("open")
-    night.classList.toggle("open")
-    document.querySelector(".navColor i").classList.toggle("fa-spin")
-})
+    navOpen.classList.toggle("open");
+    night.classList.toggle("open");
+    document.querySelector(".navColor i").classList.toggle("fa-spin");
+});
 
 span.forEach(function (e) {
-    e.addEventListener("click", function (s) {
-        document.documentElement.style.setProperty("--dark-orange", this.dataset.color)
-        console.log(this)
-        localStorage.setItem("color", this.dataset.color)
-    })
-})
+    e.addEventListener("click", function () {
+        document.documentElement.style.setProperty("--dark-orange", this.dataset.color);
+        localStorage.setItem("color", this.dataset.color);
+    });
+});
 
 night.addEventListener("click", function () {
+    body.classList.toggle("darkMode");
+    localStorage.setItem("nightMode", body.classList.contains("darkMode") ? "darkMode" : "NotdarkMode");
+});
 
-    body.classList.toggle("darkMode")
-    if (body.className === "darkMode") {
-        localStorage.setItem("nightMode", "darkMode")
-    } else {
-        console.log("NotdarkMode")
-        localStorage.setItem("nightMode", "NotdarkMode")
+// Text animation for promotional text
+let textInnx = document.querySelector(".texttype");
+let x = "Supper delicious Burger in town!";
+let i = 0;
 
-    }
-})
-let textInnx = document.querySelector(".texttype")
-let se = document.querySelector(".textAbout h2")
-let x = "Supper delicious Burger in town!"
-let i = 0
 function animation() {
     if (i < x.length) {
-        textInnx.innerHTML += x.charAt(i)
-        i++
-
+        textInnx.innerHTML += x.charAt(i);
+        i++;
     } else {
-        textInnx.innerHTML = ""
-        i = -1
-        i++
+        textInnx.innerHTML = "";
+        i = 0;
     }
-    setTimeout(animation, 300)
+    setTimeout(animation, 300);
 }
-animation()
+animation();
 
-
+// Click event to remove image open class
 body.addEventListener("click", function (img) {
-    img.target.classList.remove("openImg")
-
-    if (img.target.classList == "slid") {
-        img.target.parentElement.classList.toggle("openImg")
-
+    img.target.classList.remove("openImg");
+    if (img.target.classList.contains("slid")) {
+        img.target.parentElement.classList.toggle("openImg");
     }
-})
+});
 
-
-
-function loader(){
-    document.querySelector(".loading").style.display ="none"
-}
-function fadeOut(){
-    setInterval(loader ,10000)
+// Loader function to hide loading screen after window load
+function loader() {
+    document.querySelector(".loading").style.display = "none";
 }
 
-window.onload = fadeOut()
+function fadeOut() {
+    setInterval(loader, 10000);
+}
+
+window.onload = fadeOut();
